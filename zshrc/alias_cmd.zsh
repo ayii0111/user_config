995caaa76cde3f77ac7c4f2da651a5285e9b3a5c
ayii <a0913873707@gmail.com>
2024-09-29 11:30:01 +0800

    添加環境變數解決 superfile 工具錯位問題

--- zshrc/alias_cmd.zsh
+++ zshrc/alias_cmd.zsh
@@ -1,46 +1,49 @@
 # 指令別名
 # 要補全別名，要重新啟動 Fig軟件
 
-# 基本指令相關
-alias bat='bat --paging=never'
-alias ls='eza -1 --icons -s new'
+# ------------------------------ 基本指令相關 ------------------------------
+alias bat='bat --paging=never'   # cat 強化版
+alias ls='eza -1 --icons -s new' # ls 強化版(覆蓋原 ls)
 alias lsa='eza -a -1 --icons -s new'
-alias mk='mkdir -p'
-alias man='tldr'
-alias l='clear'
+alias mk='mkdir -p' # 可建立多層目錄
+alias man='tldr'    # man 強化版(覆蓋原 man)
+alias l='clear'     # 清除畫面
 
 # Dirs
-alias ..='cd ..'
-alias ...='cd ../..'
+alias ..='cd ..'     # cd 上一層
+alias ...='cd ../..' # cd 上兩層
 
-# 實用別名
+# ------------------------------ 實用別名 ------------------------------
 alias nv='nvim'
-alias rst='mac restart'
-alias cpf='pbcopy <'
-alias -g cpo='| pbcopy'
-alias zb='z -b'
+alias rst='mac restart'                                                 # mac 重開機
+alias lo="osascript -e 'tell application \"System Events\" to log out'" # mac 登出
+alias cpf='pbcopy <'                                                    # 複製檔案內容
+alias -g cpo='| pbcopy'                                                 # 複製指令輸出
+alias zb='z -b'                                                         # z 的退回祖輩目錄
+alias -g peco='| /opt/homebrew/bin/peco'                                # grep強化版，可篩選輸出結果
 # alias     con='conda'
 alias ytd='/opt/homebrew/Caskroom/miniforge/base/bin/yt-dlp'
-alias m='micro'
-alias tm='tmux'
-alias tml='tmux ls'
-alias tmn='tmux new-session -s'
-alias tmnd='tmux new-session -d -s'
-alias tma='tmux attach-session -t'
-alias tmk='tmux kill-session'
-alias tmka='tmux kill-session -a'
-alias tmks='tmux kill-server'
-alias ze='zellij'
+alias m='micro' # 文本編輯器
+
+# tmux (目前沒在用)
+# alias tm='tmux'
+# alias tml='tmux ls'
+# alias tmn='tmux new-session -s'
+# alias tmnd='tmux new-session -d -s'
+# alias tma='tmux attach-session -t'
+# alias tmk='tmux kill-session'
+# alias tmka='tmux kill-session -a'
+# alias tmks='tmux kill-server'
+alias ze='zellij' # 終端視窗管理工具
 
 # 少用別名
-alias wget='aria2c -x 16 -s 16 --retry-wait=1'
-alias vsextinstall="cat $HOME/UserConfig/vsCode/extensionsList | xargs -n 1 code --install-extension"
-alias ccg='c ~/UserConfig/config/Customize\ ChatGPT' # customize chat gpt
+alias wget='aria2c -x 16 -s 16 --retry-wait=1'                                                        # wget 強化版
+alias vsextinstall="cat $HOME/UserConfig/vsCode/extensionsList | xargs -n 1 code --install-extension" # 批量安裝 vscode 插件列表
+alias ccg='c ~/UserConfig/config/Customize\ ChatGPT'                                                  # customize chat gpt 打開自訂的 ChatGPT 的環境檔
+alias firr='npx firebase deploy --only firestore:rules'                                               # firebase 部署
 
-# npm
-alias npi='npm init -y'
-alias npr='npm run'
-alias npc='npm create'
+# ------------------------------ npm ------------------------------
+alias npi='npm init -y' # 快速 npm 初始化
+alias npr='npm run'     # npm run 的縮簡
+alias npc='npm create'  # npm create 的縮簡
 alias pn='pnpm'
-
-alias firr='npx firebase deploy --only firestore:rules'
--- zshrc/zshrc_end.zsh
+++ zshrc/zshrc_end.zsh
@@ -1,10 +1,7 @@
-
-
 # setopt配置（即 shell行為設定）
-setopt interactivecomments  # 可在shell對話中使用註解
-setopt nonomatch  # 使用*而沒有匹配時，不會報錯
-setopt EXTENDED_GLOB  # 啟用強化萬用字元
-
+setopt interactivecomments # 可在shell對話中使用註解
+setopt nonomatch           # 使用*而沒有匹配時，不會報錯
+setopt EXTENDED_GLOB       # 啟用強化萬用字元
 
 # eval "$(zoxide init zsh)"
 # eval "$(starship init zsh)"
@@ -14,12 +11,11 @@ eval "$(lua $(brew --prefix z.lua)/share/z.lua/z.lua --init zsh)"
 # 下面刪除整行的熱鍵，會對自動補全造成 bug
 # bindkey '^J' backward-kill-line
 
-
+export RUNEWIDTH_EASTASIAN=0 # 可解決 superfile 工具面板錯位的問題
 export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"
 export PATH=$PATH:$(go env GOPATH)/bin
 export CPPFLAGS="-I/opt/homebrew/opt/openjdk/include"
 export JAVA_HOME='/opt/homebrew/Cellar/openjdk/21.0.2/libexec/openjdk.jdk/Contents/Home'
-
 
 # 歷史命令配置
 # 抄來的，改天有再研究，再補上影片來源
@@ -37,18 +33,15 @@ setopt hist_find_no_dups
 
 # yazi 的額外功能（使用函式來封裝）
 function yy() {
-	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
-	yazi "$@" --cwd-file="$tmp"
-	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-		cd -- "$cwd"
-	fi
-	rm -f -- "$tmp"
+  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+  yazi "$@" --cwd-file="$tmp"
+  if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+    cd -- "$cwd"
+  fi
+  rm -f -- "$tmp"
 }
 
-
-
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-
 
 # pnpm
 export PNPM_HOME="/Users/ayii/Library/pnpm"
@@ -58,32 +51,23 @@ case ":$PATH:" in
 esac
 # pnpm end
 
-
 # 暫不啟用 zellij (距分割視窗的功能，但滑鼠選取變難用了)
 # eval "$(zellij setup --generate-auto-start zsh)"
 
 # 套用了 pure 主題，且 pure 也有個人化修改
 eval "$(oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/pure.omp.json)"
 
-
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/opt/homebrew/Caskroom/miniforge/base/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+__conda_setup="$('/opt/homebrew/Caskroom/miniforge/base/bin/conda' 'shell.zsh' 'hook' 2>/dev/null)"
 if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
+  eval "$__conda_setup"
 else
-    if [ -f "/opt/homebrew/Caskroom/miniforge/base/etc/profile.d/conda.sh" ]; then
-        . "/opt/homebrew/Caskroom/miniforge/base/etc/profile.d/conda.sh"
-    else
-        export PATH="/opt/homebrew/Caskroom/miniforge/base/bin:$PATH"
-    fi
+  if [ -f "/opt/homebrew/Caskroom/miniforge/base/etc/profile.d/conda.sh" ]; then
+    . "/opt/homebrew/Caskroom/miniforge/base/etc/profile.d/conda.sh"
+  else
+    export PATH="/opt/homebrew/Caskroom/miniforge/base/bin:$PATH"
+  fi
 fi
 unset __conda_setup
 # <<< conda initialize <<<
-
-
-
-
-
-
-
